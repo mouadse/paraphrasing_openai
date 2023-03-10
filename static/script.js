ed2f94b682c46f53c5055f108a5a76982430b366
mouadse <mouadse@gmail.com>
2023-03-10 03:33:51 +0100

    Deleted some css and javascript files.

--- static/script.js
+++
@@ -1,6 +0,0 @@
-const darkModeToggle = document.getElementById('dark-mode-toggle');
-const body = document.querySelector('body');
-
-darkModeToggle.addEventListener('change', function () {
-    body.classList.toggle('dark-mode');
-});
